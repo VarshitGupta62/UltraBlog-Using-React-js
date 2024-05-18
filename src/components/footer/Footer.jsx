9a3bc39db21ecdd5034d3a00684fcc5ec623052a
varshit gupta <varshitguta463@gmail.com>
2024-05-18 19:11:42 +0530

    react complex project

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
      <>
      <footer className="p-4 my-6 mx-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 xl:p-8 dark:bg-gray-800">
         <ul className="flex flex-wrap items-center mb-6 space-y-1 md:mb-0">
-            <li><Link to={'#'} className="mr-4 text-sm font-normal text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Terms and conditions</Link></li>
+            <li><Link to={'*'} className="mr-4 text-sm font-normal text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Terms and conditions</Link></li>
             <li><Link to={'#'} className="mr-4 text-sm font-normal text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Privacy Policy</Link></li>
             <li><Link to={'#'} className="mr-4 text-sm font-normal text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Licensing</Link></li>
             <li><Link to={'#'} className="mr-4 text-sm font-normal text-gray-500 hover:underline md:mr-6 dark:text-gray-400">Cookie Policy</Link></li>
